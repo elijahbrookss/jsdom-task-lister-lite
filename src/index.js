688a206493821aa298f8a5da0559135009ec7587
elijahbrookss <elijahcbrooks@gmail.com>
2020-11-18 15:27:13 -0600

    done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  createTask();
 });
+
+function createTask(){
+  submitButton();
+}
+
+function submitButton(){
+  const taskForm = document.querySelector("#create-task-form");
+  const tasksList = document.querySelector("#tasks");
+  const inputField = document.querySelector("#new-task-description");
+
+  taskForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const userInput = inputField.value + "  ";
+    const liElement = document.createElement("li");
+
+    liElement.textContent = userInput;
+    tasksList.appendChild(liElement);
+    deleteButton(liElement);
+
+    taskForm.reset();
+  });
+}
+
+function deleteButton(liElement){
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+
+  liElement.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", function(event) {
+    liElement.remove();
+  });
+}
